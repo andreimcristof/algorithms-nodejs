e93e665f1414dfc5d6d531736a5abcaa8fa9ce1b
andrei <andreimcristof@gmail.com>
2021-02-11 22:32:13 +0100

    _

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,51 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// const capitalizeWord = (word) => {
+//   let t = [];
+
+//   for (const [index, char] of word.split("").entries()) {
+//     t.push(index == 0 ? char.toUpperCase() : char);
+//   }
+//   return t.join("");
+// };
+
+// function capitalize(str) {
+//   const sep = " ";
+//   const words = str.split(sep);
+//   let cwords = [];
+//   for (const w of words) {
+//     cwords.push(capitalizeWord(w));
+//   }
+//   return cwords.join(sep);
+// }
+
+// const capitalizeWord = (word) => {
+//   const first = word[0].toUpperCase();
+//   const rest = word.slice(1);
+//   return first + rest;
+// };
+
+// function capitalize(str) {
+//   const sep = " ";
+//   const words = str.split(sep);
+//   let cwords = [];
+//   for (const w of words) {
+//     cwords.push(capitalizeWord(w));
+//   }
+//   return cwords.join(sep);
+// }
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    const element = str[i];
+    if (str[i - 1] != " ") result += str[i];
+    else result += str[i].toUpperCase();
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,23 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+const stepLine = (hashes, spaces) => {
+  let result = [];
+
+  for (let i = 0; i < hashes; i++) {
+    result.push("#");
+  }
+
+  for (let j = 0; j < spaces; j++) {
+    result.push(" ");
+  }
+  return result.join("");
+};
+
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(stepLine(i, n - i));
+  }
+}
 
 module.exports = steps;
