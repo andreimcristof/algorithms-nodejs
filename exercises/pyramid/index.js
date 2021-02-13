d2011efed79a993b9869904f36e8c7ba01daddd7
andrei <andreimcristof@gmail.com>
2021-02-13 18:03:52 +0100

    _

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,73 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######'
 
-function pyramid(n) {}
+// const chars = (n, char) => {
+//   let t = "";
+//   for (let i = 1; i <= n; i++) {
+//     t += char;
+//   }
+//   return t;
+// };
+
+// function pyramid(n) {
+//   for (let row = 1; row <= n; row++) {
+//     const totalLength = row + (row - 1);
+//     if (row === 1 || row == n) {
+//       hashCount = totalLength;
+//     } else hashCount == row - 1;
+
+//     const spaceCount = totalLength - hashCount;
+
+//     // if it's first or last row, totalLength === hashcount
+//     let line = "";
+//     console.log("total", totalLength, "spaces", spaceCount, "hashes", hashCount);
+//     line = chars(Math.floor(spaceCount / 2), " ") + chars(hashCount, "#") + chars(Math.floor(spaceCount / 2), " ");
+//     console.log(line);
+//   }
+// }
+
+// function pyramid(n) {
+//   const total = 2 * n - 1;
+//   const center = Math.floor(total / 2);
+
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+//     for (let column = 0; column < total; column++) {
+//       // console.log("center", center, "row", row, "column", column);
+//       if (center - row <= column && center + row >= column) level += "#";
+//       else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+function pyramid(n, row = 0, level = "") {
+  const totalRowLength = 2 * n - 1;
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === totalRowLength) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor(totalRowLength / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  pyramid(n, row, level + add);
+}
 
 module.exports = pyramid;
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,23 +17,52 @@
 //       '### '
 //       '####'
 
-const stepLine = (hashes, spaces) => {
-  let result = [];
+// const stepLine = (hashes, spaces) => {
+//   let result = [];
 
-  for (let i = 0; i < hashes; i++) {
-    result.push("#");
+//   for (let i = 0; i < hashes; i++) {
+//     result.push("#");
+//   }
+
+//   for (let j = 0; j < spaces; j++) {
+//     result.push(" ");
+//   }
+
+//   return result.join("");
+// };
+
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(stepLine(i, n - i));
+//   }
+// }
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) stair += "#";
+//       else stair += " ";
+//     }
+//     console.log(stair);
+//   }
+// }
+
+function steps(n, row = 0, stair = "") {
+  if (n === row) return;
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
   }
 
-  for (let j = 0; j < spaces; j++) {
-    result.push(" ");
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
   }
-  return result.join("");
-};
 
-function steps(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log(stepLine(i, n - i));
-  }
+  steps(n, row, stair);
 }
 
 module.exports = steps;
