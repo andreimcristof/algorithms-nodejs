d951926f544763828a45f94c2144999c964983b5
andrei <andreimcristof@gmail.com>
2021-02-28 13:43:25 +0100

    _

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,65 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      // console.log("arr", arr);
+      // console.log("j+1", arr[j + 1], "j", arr[j]);
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        // we found an element with lesser value. we update indexOfMin to look at it now
+        indexOfMin = j;
+      }
+    }
+    // if it was swapped during iteration, it means a lesser one was found, so it needs to be swapped
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) return arr;
 
+  const center = Math.floor(arr.length / 2);
+  let left = arr.slice(0, center);
+  let right = arr.slice(center);
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  let results = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      const firstLeft = left.shift();
+      results.push(firstLeft);
+    } else {
+      const firstRight = right.shift();
+      results.push(firstRight);
+    }
+  }
 
+  // if anyting is left in these arrays just push the remaining elements in results
+  // the order doesnt matter, adding left vs right first, because one of them will always be empty (due to the while which was stopping only then)
+  if (left.length > 0) results = [...results, ...left];
+  if (right.length > 0) results = [...results, ...right];
+  return results;
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
--- runtime-complexity.md
+++ runtime-complexity.md
@@ -131,3 +131,11 @@ nodeOne.next = nodeTwo;
 - the term "search" in BST enforces requirements on the data contained in the nodes, that it respects these conditions. If we remove the term "search" and we have Binary Trees, these can contain data that does not fit in the strict requirements for each parent nodes.
 - in an interview setting you should ask if its a BST (restricted data on child nodes) or a BT (no data restriction, anything goes).
 - another common question in an interview setting is "validate" that this is a well formed BST (e..g in this particular case every node has 2 children, and left child nodes all are less than parent, and right child nodes are always greater than parent)
+
+## Sorting
+
+- BubbleSort - n^2 quadratic complexity - easiest
+- SelectionSort - n^2 quadratic complexity - easier
+- MergeSort - n\*log(n) - medium
+
+- so BubbleSort and SelectionSort are v slow for large collections, but good at small collections (e.g. < 100 elements)
